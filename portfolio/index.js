cf85ba58de7664e333f9c41fdd4c79113ba84333
EArehon <earehon@gmail.com>
2022-01-18 17:23:56 +0300

    feat: add change image in portfolio

--- portfolio/index.js
+++ portfolio/index.js
@@ -20,3 +20,40 @@ function closeMenu(event){
   
   }
 }
+
+
+const portfolioBtn = document.querySelector('.buttonSwitch1');
+const portfolioImages = document.querySelectorAll('.portfolioImage');
+
+//portfolioBtn.addEventListener('click', () => {portfolioImages.forEach((img, index) => img.src = `./assets/img/winter/${index + 1}.jpg`)});
+
+const portfolioBtns = document.querySelector('.buttonWrapper');
+
+function changeImage(event) {
+  if(event.target.classList.contains('buttonSwitch')){
+    //portfolioBtns.forEach(element => element.classList.remove('active'));
+    portfolioImages.forEach((img, index) => img.src = `./assets/img/${event.target.dataset.season}/${index + 1}.jpg`);
+  }
+}
+
+function changeClassActive(){
+
+}
+
+portfolioBtns.addEventListener('click', changeImage);
+
+const seasons = ['winter', 'spring', 'summer', 'autumn'];
+
+seasons.forEach( element => preloadImages(element));
+
+function preloadImages(season){
+  for(let i = 0; i <= 6; i++) {
+    const img = new Image();
+    img.src = `./assets/img/${season}/${i}.jpg`;
+  }
+}
+
+
+
+
+
