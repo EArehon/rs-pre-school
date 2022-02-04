47867fbc888a7e8b343c487ddbe5ad9f505f17e5
EArehon <earehon@gmail.com>
2022-02-04 20:59:42 +0300

    init: start task

---
+++ random-jokes/index.js
@@ -0,0 +1,12 @@
+const url = 'https://type.fit/api/quotes';
+const quotes = './quotes.json';
+const quote = document.querySelector('.quote');
+
+async function getData() {
+    const res = await fetch(quotes);
+    const data = await res.json();
+    //console.log(data.length);
+    quote.textContent = data[1].text;
+    //console.log(data[1].text);
+  }
+  getData();
